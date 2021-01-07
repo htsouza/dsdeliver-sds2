473b5aa9319209b3b0f79c10807f93799d85f5dd
Hugo Souza <hugosouza.si@gmail.com>
2021-01-06 23:07:41 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './Home/Index';
 import Navbar from './Navbar/Index';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
