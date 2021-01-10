7ebf39125b9aa1deacd79472d9f825b5bef13655
Hugo Souza <hugosouza.si@gmail.com>
2021-01-10 14:57:10 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -88,6 +88,14 @@ public class Order implements Serializable {
         this.status = status;
     }
 
+    public Double getTotal() {
+        double sum = 0.0;
+        for (Product product: products) {
+            sum += product.getPrice();
+        }
+        return sum;
+    }
+
     public Set<Product> getProducts() {
         return products;
     }
