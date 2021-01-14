97060e31cc95673d9ea18280ac2e957c2d50463d
Hugo Souza <hugosouza.si@gmail.com>
2021-01-13 23:17:55 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,11 +1,9 @@
+import React from 'react';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Header from './src/Header';
-import Home from './src/Home';
-
+import Routes from './src/Routes';
 
 export default function App() {
 
@@ -21,8 +19,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home />
+      <Routes />
     </View>
   );
 }
---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
+import Header from '../Header';
+import OrderCard from '../OrderCard';
+
+function Orders() {
+    const handleOnPress = () => {
+
+    }
+
+    return (
+        <>
+            <Header />
+            <ScrollView style={styles.container}>
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+            </ScrollView>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingRight: '5%',
+        paddingLeft: '5%',       
+    }
+});
+
+export default Orders;
