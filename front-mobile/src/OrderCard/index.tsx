7bb28ba1eeab3abe552091d018e1161bf5056be7
Hugo Souza <hugosouza.si@gmail.com>
2021-01-14 21:35:24 -0300

    Add GPS Integration

--- front-mobile/src/OrderCard/index.tsx
+++ front-mobile/src/OrderCard/index.tsx
@@ -1,23 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Order } from '../typs';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import "intl";
+import "intl/locale-data/jsonp/pt-BR.js";
+import { Platform } from "react-native";
 
-function OrderCard() {
-    const handleOnPress = () => {
-
+if (Platform.OS === "android") {
+    if (typeof (Intl as any).disableRegExpRestore === "function") {
+        (Intl as any).disableRegExpRestore();
     }
+}
 
+dayjs.locale('pt-br');
+dayjs.extend(relativeTime);
+
+type Props = {
+    order: Order;
+}
+
+function dateFromNow(date: string) {
+    return dayjs(date).fromNow();
+}
+
+export function formatPrice(price: number) {
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2
+    });
+
+    return formatter.format(price);
+}
+
+function OrderCard({ order }: Props) {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Text style={styles.orderName}>Pedido 1</Text>
-                <Text style={styles.orderPrice}>R$ 50,00</Text>
+                <Text style={styles.orderName}>Pedido {order.id}</Text>
+                <Text style={styles.orderPrice}>{formatPrice(order.total)}</Text>
             </View>
-            <Text style={styles.text}>Há 30 min</Text>
-            <View style={styles.productsList}>
-                <Text style={styles.text}>Pizza Calabresa</Text>
-                <Text style={styles.text}>Pizza Quatro Queijos</Text>
-                <Text style={styles.text}>Pizza Marguerita</Text>
-            </View>
+            <Text style={styles.text}>{dateFromNow(order.moment)}</Text>
+            <ScrollView style={styles.productsList}>
+                {order.products.map(product =>
+                    <Text key={product.id} style={styles.text}>{product.name}</Text>
+                )}
+            </ScrollView>
         </View>
     );
 }
--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,22 +1,57 @@
-import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, ScrollView, Alert, Text } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { fecthOrders } from '../api';
 import Header from '../Header';
 import OrderCard from '../OrderCard';
+import { Order } from '../typs';
 
 function Orders() {
-    const handleOnPress = () => {
 
+    const [orders, setOrders] = useState<Order[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigation = useNavigation();
+    const isFocused = useIsFocused();
+
+    const fetchData = () => {
+        setIsLoading(true);
+        fecthOrders()
+        .then(response => setOrders(response.data))
+        .catch(() => Alert.alert('Houve um erro ao buscar os pedidos'))
+        .finally(() => setIsLoading(false));
+    }
+
+    useEffect(() => {
+        if (isFocused) {
+            fetchData();
+        }
+    }, [isFocused]);
+
+
+    const handleOnPress = (order: Order) => {
+        navigation.navigate('OrderDetails', {
+            order
+        });
     }
 
     return (
         <>
             <Header />
             <ScrollView style={styles.container}>
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
+                {isLoading ? (
+                    <Text>Buscando pedidos...</Text>
+                ) : (
+                    (orders.length > 0 ?
+                    orders.map(order => (
+                        <TouchableWithoutFeedback 
+                            key={order.id} 
+                            onPress={() => handleOnPress(order)}
+                        >
+                            <OrderCard order={order}/>
+                        </TouchableWithoutFeedback>
+                    )) : <Text>Não existem pedidos para serem entregues!</Text>)
+                )}                
             </ScrollView>
         </>
     );
