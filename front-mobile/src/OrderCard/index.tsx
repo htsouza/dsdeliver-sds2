156d18ea43386d2f6f0969f688d17cd025cf7ba7
Hugo Souza <hugosouza.si@gmail.com>
2021-01-20 20:18:36 -0300

    Fix OrderCard Style

--- front-mobile/src/OrderCard/index.tsx
+++ front-mobile/src/OrderCard/index.tsx
@@ -54,14 +54,18 @@ function OrderCard({ order }: Props) {
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: '10%',
+        marginTop: '2%',
+        marginLeft: '2%',
+        marginRight: '2%',
+        marginBottom: '2%',
         padding: 15,
         backgroundColor: '#FFF',
         shadowOpacity: 0.25,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 4 },
         shadowRadius: 20,
-        borderRadius: 10
+        borderRadius: 10,
+        elevation: 5
     },
     header: {
         flexDirection: 'row',
