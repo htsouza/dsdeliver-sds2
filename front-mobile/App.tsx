f0605433710bf7f0f6ad1af3fdc29221385be66b
Hugo Souza <hugosouza.si@gmail.com>
2021-01-20 20:54:40 -0300

    Add fix OpenSans font error

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Routes from './src/Routes';
@@ -8,8 +8,8 @@ import Routes from './src/Routes';
 export default function App() {
 
   let [fontsLoaded] = useFonts({
-    OpenSans_400Regular,
-    OpenSans_700Bold
+    OpenSans_400Regular, 
+    OpenSans_700Bold,
   });
 
   if (!fontsLoaded) {
